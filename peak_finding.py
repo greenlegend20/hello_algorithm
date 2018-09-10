d6c61b9f22c46dcb966665c374e34642993cca54
greenlegend20 <gwx0731@gmail.com>
2018-09-10 13:56:22 +0900

    add bubble insertion selection merge sort

--- peak_finding.py
+++ peak_finding.py
@@ -1,10 +1,7 @@
-import random
-
-
 def find_any_peak_1d(a):
     if len(a) <= 2:
         return max(a)
-    start = int(len(a) / 2)
+    start = len(a) // 2
     if a[start] < a[start - 1]:
         return find_any_peak_1d(a[:start])
     elif a[start] < a[start + 1]:
@@ -16,7 +13,7 @@ def find_any_peak_1d(a):
 def find_any_peak_2d(a):
     if len(a) <= 2:
         return max([max(row) for row in a])
-    start_row = int(len(a) / 2)
+    start_row = len(a) // 2
     start_col = a[start_row].index(max(a[start_row]))
     if a[start_row][start_col] < a[start_row - 1][start_col]:
         return find_any_peak_2d(a[:start_row])
@@ -24,8 +21,3 @@ def find_any_peak_2d(a):
         return find_any_peak_2d(a[start_row+1:])
     else:
         return a[start_row][start_col]
-
-
-matrix = [[random.randint(10, 100) for i in range(5)] for j in range(5)]
-[print(row) for row in matrix]
-print(find_any_peak_2d(matrix))
