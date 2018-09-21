3a4a2c75344d3666686d75b37cf5a708f4948af7
greenlegend20 <gwx0731@gmail.com>
2018-09-21 15:12:45 +0900

    add heap coutning radix sort

--- sorting.py
+++ sorting.py
@@ -1,13 +1,14 @@
-def bubble_sort(a, ascending=True):
-    result = a.copy()
-    for i in range(len(result) - 1, 0, -1):
+def bubble_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    for i in range(len(a) - 1, 0, -1):
         for j in range(i):
-            if (ascending and result[j] > result[j + 1]) or (not ascending and result[j] < result[j + 1]):
-                result[j], result[j + 1] = result[j + 1], result[j]
-    return result
+            if (ascending and a[j] > a[j + 1]) or (not ascending and a[j] < a[j + 1]):
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a
 
 
-def merge_sort(a, ascending=True):
+def merge_sort(a, ascending=True, inplace=True):
     if len(a) <= 1:
         return a
     middle = len(a) // 2
@@ -27,27 +28,101 @@ def merge_sort(a, ascending=True):
         result += right_list
     else:
         result += left_list
+    if inplace:
+        for i in range(len(a)):
+            a[i] = result[i]
     return result
 
 
-def insertion_sort(a, ascending=True):
-    result = a.copy()
-    for i in range(1, len(result)):
+def insertion_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    for i in range(1, len(a)):
         j = i - 1
-        next_element = result[i]
-        while ((ascending and result[j] > next_element) or (not ascending and result[j] < next_element)) and j >= 0:
-            result[j + 1] = result[j]
+        next_element = a[i]
+        while ((ascending and a[j] > next_element) or (not ascending and a[j] < next_element)) and j >= 0:
+            a[j + 1] = a[j]
             j -= 1
-        result[j + 1] = next_element
-    return result
+        a[j + 1] = next_element
+    return a
 
 
-def selection_sort(a, ascending=True):
-    result = a.copy()
-    for i in range(len(result)):
+def selection_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    for i in range(len(a)):
         m_i = i
-        for j in range(i+1, len(result)):
-            if (ascending and result[m_i] > result[j]) or (not ascending and result[m_i] < result[j]):
+        for j in range(i+1, len(a)):
+            if (ascending and a[m_i] > a[j]) or (not ascending and a[m_i] < a[j]):
                 m_i = j
-        result[i], result[m_i] = result[m_i], result[i]
-    return result
+        a[i], a[m_i] = a[m_i], a[i]
+    return a
+
+
+def heap_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    def heapify(h, i, end): # stop at end (exclusive)
+        l = 2 * i + 1
+        r = 2 * i + 2
+        m = i
+        if l < end and ((ascending and h[i] < h[l]) or (not ascending and h[i] > h[l])):
+            m = l
+        if r < end and ((ascending and h[m] < h[r]) or (not ascending and h[m] > h[r])):
+            m = r
+        if m != i:
+            h[i], h[m] = h[m], h[i]
+            heapify(h, m, end)
+
+    start = len(a) // 2 - 1
+    for i in range(start, -1, -1): # build heap
+        heapify(a, i, len(a))
+    for i in range(len(a) - 1, 0, -1):
+        a[0], a[i] = a[i], a[0]
+        heapify(a, 0, i)
+    return a
+
+
+def counting_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    assert min(a) >= 0
+    assert max(a) <= 1000000
+    count = [0] * (max(a) + 1)
+    for i in a:
+        count[i] += 1
+    i = 0
+    for j in (range(len(count)) if ascending else reversed(range(len(count)))):
+        for c in range(count[j]):
+            a[i] = j
+            i += 1
+    return a
+
+
+def radix_sort(a, ascending=True, inplace=True):
+    if not inplace:
+        a = a[:]
+    def get_digit(n, d):
+        for i in range(d-1):
+            n //= 10
+        return n % 10
+    def get_num_digit(n):
+        i = 0
+        while n > 0:
+            n //= 10
+            i += 1
+        return i
+
+    m = max(a)
+    assert min(a) >= 0
+    assert m <= 1000000
+    for d in range(1, get_num_digit(m)+1):
+        count = [[] for _ in range(10)]
+        for num in a:
+            count[get_digit(num, d)].append(num)
+        i = 0
+        for j in (range(10) if ascending else reversed(range(10))):
+            for num in count[j]:
+                a[i] = num
+                i += 1
+    return a
